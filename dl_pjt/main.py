5e7eae1afd8ef3f44dcdfcb75bf53a48f9d11ffb
seongbae15 <pseongbae@gmail.com>
2022-07-08 15:06:05 +0900

    feat: load dataset
    load data & convert img data(str->np.array)

--- dl_pjt/main.py
+++ dl_pjt/main.py
@@ -1,9 +1,12 @@
 import tensorflow
-from utils import load_dataset
+from utils import get_dataset_xy
+from utils import split_train_valid
+
 
 def main():
-    train_x, train_y = load_dataset(is_train=True)
-
+    train_x, train_y = get_dataset_xy(is_train=True)
+    #split train valid
+    split_train_valid(train_x, train_y)
     return
 
 if __name__ == "__main__":
--- dl_pjt/utils.py
+++ dl_pjt/utils.py
@@ -1,19 +1,23 @@
 import pandas as pd
 import numpy as np
-def load_dataset(is_train=True):
+
+
+def get_dataset_xy(is_train=True):
+    df = __load_dataframe(is_train)
+    y = df.iloc[:,:-1]
+    x = df.iloc[:,-1]
+    x = __convert_image_dataset(x)
+    return x, y
+
+def __load_dataframe(is_train=True):
     base_path="./data/facial-keypoints-detection/"
     if is_train:
         file_name = "training.csv"
     else:
         file_name = "test.csv"
-    df = pd.read_csv(base_path + file_name)
-    y = df.iloc[:,:-1]
-    x = df.iloc[:,-1]
-    __convert_image_dataset(x)
-    return x, y
-
+    return pd.read_csv(base_path + file_name)
 
 def __convert_image_dataset(raw_image_infos):
-    image_infos = [raw_image_info.split() for raw_image_info in raw_image_infos]
-    print(np.array(image_infos).shape)
+    image_infos= np.array([list(map(int, raw_img_info.split())) for raw_img_info in raw_image_infos]).reshape(-1,96,96)
+    return image_infos
 
